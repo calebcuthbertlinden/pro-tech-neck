8aa8b42be55258c1a7759653902071cd92ecfde8
Caleb Linden <48081625+calebcuthbertlinden@users.noreply.github.com>
2019-08-15 19:06:58 +0200

    feature/move-sensor-logic-to-util

--- app/src/main/java/com/example/protechneck/MainActivity.java
+++ app/src/main/java/com/example/protechneck/MainActivity.java
@@ -1,44 +1,27 @@
 package com.example.protechneck;
 
-import android.app.ActivityManager;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.example.protechneck.Util.NeckCheckerService;
-import com.example.protechneck.Util.NeckCheckerServiceTemp;
+import com.example.protechneck.Util.SensorUtil;
 
 public class MainActivity extends AppCompatActivity {
 
     Intent mServiceIntent;
-    private NeckCheckerService mSensorService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mSensorService = new NeckCheckerService(this);
+        NeckCheckerService mSensorService = new NeckCheckerService();
         mServiceIntent = new Intent(this, mSensorService.getClass());
-        if (!isMyServiceRunning(mSensorService.getClass())) {
+        if (!SensorUtil.isMyServiceRunning(mSensorService.getClass(), this)) {
             startService(mServiceIntent);
         }
         this.startService(mServiceIntent);
     }
-
-    private boolean isMyServiceRunning(Class<?> serviceClass) {
-        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-            if (serviceClass.getName().equals(service.service.getClassName())) {
-                Log.i ("isMyServiceRunning?", true+"");
-                return true;
-            }
-        }
-        Log.i ("isMyServiceRunning?", false+"");
-        return false;
-    }
-
 
     protected void onResume() {
         super.onResume();
--- app/src/main/java/com/example/protechneck/Util/TechNeckActivity.java
+++ app/src/main/java/com/example/protechneck/Util/TechNeckActivity.java
@@ -1,11 +1,13 @@
 package com.example.protechneck.Util;
 
-import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -13,10 +15,14 @@ import com.example.protechneck.Models.PostureAnalyticsEvent;
 import com.example.protechneck.Models.PostureEventType;
 import com.example.protechneck.R;
 
-public class TechNeckActivity extends AppCompatActivity {
+public class TechNeckActivity extends AppCompatActivity implements SensorEventListener {
 
     private TextView postureType;
     private ConstraintLayout container;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private Sensor magneticField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,41 +32,80 @@ public class TechNeckActivity extends AppCompatActivity {
         postureType = findViewById(R.id.posture_type);
         container = findViewById(R.id.container);
 
+        initSensors();
+        validateAndRegisterListener(accelerometer, Sensor.TYPE_ACCELEROMETER);
+        validateAndRegisterListener(magneticField, Sensor.TYPE_MAGNETIC_FIELD);
 
-        PostureEventType viewType = PostureEventType.valueOf(getIntent().getStringExtra("VIEW_TYPE_EXTRA"));
+        determineAction(PostureEventType.valueOf(getIntent().getStringExtra("VIEW_TYPE_EXTRA")));
+    }
+
+    private void initSensors() {
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        SensorUtil.setRotationMatrix();
+        SensorUtil.setOrientation();
+    }
+
+    /**
+     *
+     * @param event any event triggered by a sensor with a listener attached
+     */
+    public void onSensorChanged(SensorEvent event) {
+        determineAction(SensorUtil.determineViewTypeUsingPitch(
+                SensorUtil.getOrientationValue(event, 1)));
+    }
+
+    /**
+     *
+     * @param sensor the sensor to attach the listener to
+     * @param type the type of sensor we want to attach a listener to
+     */
+    private void validateAndRegisterListener(Sensor sensor, int type) {
+        if (sensor != null) {
+            sensorManager.registerListener(this, sensor,
+                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            SensorUtil.logUnavailableSensor(type);
+        }
+    }
+
+    /**
+     *
+     * @param viewType
+     */
+    private void determineAction(PostureEventType viewType) {
+        if (viewType == null) {
+            return;
+        }
 
         switch (viewType) {
             case FLAT_PHONE:
                 // Bad posture
                 AnalyticsUtil.sendAnalyticsEvent(PostureAnalyticsEvent.BAD_POSTURE, null, null);
-                Log.e("Posture", "BAD_POSTURE - FLAT PHONE");
-                postureType.setText("BAD POSTURE");
+                postureType.setText(getString(R.string.posture_type_bad));
                 container.setBackgroundColor(ContextCompat.getColor(this, R.color.badBackground));
                 break;
             case LOW_ANGLED:
                 // Bad posture
-                AnalyticsUtil.sendAnalyticsEvent(PostureAnalyticsEvent.BAD_POSTURE, null, null);
-                Log.e("Posture", "BAD_POSTURE - LOW ANGLED");
-                postureType.setText("OKAY POSTURE");
+                AnalyticsUtil.sendAnalyticsEvent(PostureAnalyticsEvent.OKAY_POSTURE, null, null);
+                postureType.setText(getString(R.string.posture_type_okay));
                 container.setBackgroundColor(ContextCompat.getColor(this, R.color.okayBackground));
                 break;
-            case HIGH_ANGLED:
-                // Okay posture
-                AnalyticsUtil.sendAnalyticsEvent(PostureAnalyticsEvent.OKAY_POSTURE, null, null);
-                Log.e("Posture", "OKAY_POSTURE - HIGH ANGLE");
-                postureType.setText("BETTER POSTURE");
-                container.setBackgroundColor(ContextCompat.getColor(this, R.color.badBackground));
             case PERFECT_POSTURE:
                 // Perfect posture
                 AnalyticsUtil.sendAnalyticsEvent(PostureAnalyticsEvent.PERFECT_POSTURE, null, null);
-                Log.e("Posture", "BEST_POSTURE - PERFECT POSTURE");
-                postureType.setText("PERFECT POSTURE");
+                postureType.setText(getString(R.string.posture_type_good));
                 container.setBackgroundColor(ContextCompat.getColor(this, R.color.goodBackground));
                 break;
             default:
                 break;
         }
+    }
 
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.e("MainActivity", "onAccuracyChanged: " + accuracy);
     }
 
     protected void onResume() {
