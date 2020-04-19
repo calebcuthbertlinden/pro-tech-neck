5bcdb6fc92666cf16e90eb253c3ea3c463f820b7
Caleb Linden <calebl@discovery.co.za>
2020-04-19 14:17:04 +0200

    [11][feature] add new user preference and view pager

--- app/src/main/java/com/example/protechneck/MainActivity.java
+++ app/src/main/java/com/example/protechneck/MainActivity.java
@@ -16,25 +16,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        NeckCheckerService mSensorService = new NeckCheckerService();
-        mServiceIntent = new Intent(this, mSensorService.getClass());
-        if (!SensorUtil.isMyServiceRunning(mSensorService.getClass(), this)) {
-            startService(mServiceIntent);
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+        if (isFirstTimeUser()) {
+            NeckCheckerService mSensorService = new NeckCheckerService();
+            mServiceIntent = new Intent(this, mSensorService.getClass());
+            if (!SensorUtil.isMyServiceRunning(mSensorService.getClass(), this)) {
+                startService(mServiceIntent);
+            }
+            updateSharedPref();
+            this.startService(mServiceIntent);
+            finish();
+        } else {
+            // TODO 11 - go to onboarding
         }
-        updateSharedPref();
-        this.startService(mServiceIntent);
-        finish();
     }
 
     private void updateSharedPref() {
         SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0);
         SharedPreferences.Editor editor = pref.edit();
         editor.putBoolean("TECH_NECK_RUNNING", false);
-        editor.commit();
+        editor.apply();
     }
 
-    protected void onResume() {
-        super.onResume();
+    private boolean isFirstTimeUser() {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0);
+        return pref.getBoolean("IS_RETURNING_USER", false);
     }
 
     protected void onStop() {
---
+++ app/src/main/java/com/example/protechneck/ui/SettingsActivity.java
@@ -0,0 +1,23 @@
+package com.example.protechneck.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.protechneck.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void onResume() {
+        super.onResume();
+    }
+
+    protected void onStop() {
+        super.onStop();
+    }
+}
